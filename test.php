b107b93903170e205bfbb6da4f2cb7d0121208e9
zhanglin1114 <1982404610@qq.com>
2020-05-15 16:52:51 +0800

    模拟tcp 协议发送请求

--- test.php
+++ test.php
@@ -5,16 +5,45 @@
  * Date : 2020/5/14
  * Time : 10:03
  */
-$redis = new \Redis();
-$redis->pconnect('127.0.0.1',6379);
-$redis->auth('123456');
-$redis->select(2);
-$data_json = [
-    'create_time'=>time(),
-    'user_id'=>99886,
-    'expire_time'=>7*24*60*60
-];
-$token = md5(time());
-$redis->hSet('user_token',$token,json_encode($data_json));
+//$redis = new \Redis();
+//$redis->pconnect('127.0.0.1',6379);
+//$redis->auth('123456');
+//$redis->select(2);
+//$data_json = [
+//    'create_time'=>time(),
+//    'user_id'=>99886,
+//    'expire_time'=>7*24*60*60
+//];
+//$token = md5(time());
+//$redis->hSet('user_token',$token,json_encode($data_json));
+//
+//echo $token;
+namespace Service;
 
-echo $token;
+
+class ServiceObj
+{
+    protected $serv = null;
+    public function __construct()
+    {
+        $this->serv = new \Swoole\Server('0.0.0.0',9502);
+        $this->serv->on('Connect', [$this,'onConnect']);
+        $this->serv->on('Receive', [$this,'onReceive']);
+        $this->serv->on('Close', [$this,'onClose']);
+        //启动服务器
+        $this->serv->start();
+    }
+    public function onConnect($serv, $fd)
+    {
+        echo "客户端连接成功".PHP_EOL;
+    }
+    public function onReceive($serv, $fd, $from_id, $data)
+    {
+        $serv->send($fd, "服务端发送的数据: ".$data."\n");
+    }
+    public function onClose($serv, $fd)
+    {
+        echo "客户端主动断开连接".PHP_EOL;
+    }
+}
+new ServiceObj();
